e73c94ffbfb02605cdf7c9cd4d73e125f60cbceb
duytq <duytq@topica.edu.vn>
2018-12-24 08:55:53 +0700

    innitial code

---
+++ src/main/java/quangduy/com/admin/controller/LoginController.java
@@ -0,0 +1,14 @@
+package quangduy.com.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String viewLogin(){
+
+        return "login";
+    }
+}
